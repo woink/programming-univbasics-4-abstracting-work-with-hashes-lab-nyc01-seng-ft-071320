8fe49ce9a46806f738065be7f9f5e7ac4d825a70
Ward Price <wardprice@gmail.com>
2020-06-27 19:50:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash(key => value)
+  hash = {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
